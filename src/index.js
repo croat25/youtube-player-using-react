ab091794fbcc692ab8350d6414c82b5d8684d9c7
Bruno Salapic <sala4460@mylaurier.ca>
2017-02-02 15:48:44 -0500

    added a class were a videolist is generate

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-//AIzaSyDI4zxofaNuf5wNpap6ZUgdvYsRooLYBfQ
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchbar';
+import VideoList from './components/videolist';
 const API_KEY = 'AIzaSyDI4zxofaNuf5wNpap6ZUgdvYsRooLYBfQ';
+
+
+
+
+
+
 //create a new component 
-const App=()=>{
+class App extends Component{
+	constructor (props){
+		super(props);
+
+		this.state={ videos:[] };
+
+		YTSearch({key:API_KEY, term:'surfboards'},(videos) => {
+			this.setState({videos});
+	});
+	}
+				///videos is considered a prop so thats why when u 
+
+	render(){
 	return	(
 		<div>
 			<SearchBar />
+			
+			<VideoList videos={this.state.videos} />
 	</div>
-	);
+	);}
 }
 
 
